70d94bdb0ea47e238811b91b0bad27214dddc0d9
zimmj <zimmi.94@live.de>
2025-11-09 05:47:24 +0100

    ~add: tailwind support and some example use

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  integrations: [tailwind()]
 });
