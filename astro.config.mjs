e17a684687c45d0eb58f26dfe2cfacc88e6432e6
zimmj <zimmi.94@live.de>
2026-01-04 22:04:10 +0100

    ~add: netllify toml and change astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
 });
