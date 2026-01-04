44d695d7e8a7a089f77e420f02c078608d433c41
zimmj <zimmi.94@live.de>
2026-01-04 13:25:17 +0100

    ~add: login and registration in supabase

---
+++ src/pages/api/auth/signout.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ cookies, redirect }) => {
+  cookies.delete('sb-access-token', { path: '/' });
+  cookies.delete('sb-refresh-token', { path: '/' });
+  return redirect('/signin');
+};
