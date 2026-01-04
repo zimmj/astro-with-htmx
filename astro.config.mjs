e219192160adc677bbeff4dbf166f2264c86d256
zimmj <zimmi.94@live.de>
2026-01-04 22:09:33 +0100

    ~fix: netlify deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: false
+  }),
   integrations: [tailwind()],
+  output: 'server'
 });
